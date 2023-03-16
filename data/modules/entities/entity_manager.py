90f4850d2fd0589df541f153687e172c871ba76b
Yu266426 <yu266426@gmail.com>
2023-03-16 10:18:21 -0700

    Fixed removal of entities

--- data/modules/entities/entity_manager.py
+++ data/modules/entities/entity_manager.py
@@ -1,3 +1,6 @@
+import pygame
+from pygbase import Camera
+
 from data.modules.base.constants import TILE_SIZE
 from data.modules.base.utils import get_1d_pos
 
@@ -6,13 +9,20 @@ class EntityManager:
 	def __init__(self):
 		from data.modules.entities.entity import Entity
 		self.entities: list[Entity] = []
+		self.entity_tags: dict[Entity, set[str]] = {}
 		self.sorted_entities: dict[int, list[Entity]] = {}
 		self.tagged_entities: dict[str, list[Entity]] = {}
+
+		self.entities_to_remove = set()
 
 	def add_entity(self, entity, tags: list[str] = None):
 		entity_tags = [] if tags is None else tags
 
 		self.entities.append(entity)
+		self.entity_tags[entity] = set()
+
+		for tag in entity_tags:
+			self.entity_tags[entity].add(tag)
 
 		for tag in entity_tags:
 			if tag not in self.tagged_entities:
@@ -20,36 +30,14 @@ class EntityManager:
 
 			self.tagged_entities[tag].append(entity)
 
+	def remove_entity(self, entity):
+		self.entities_to_remove.add(entity)
+
 	def get_entities_of_tag(self, tag: str):
 		if tag in self.tagged_entities:
 			return self.tagged_entities[tag]
 		else:
 			return []
-
-	def update(self, delta: float):
-		self.sorted_entities.clear()
-
-		for entity in self.entities:
-			if not entity.is_alive():
-				self.entities.remove(entity)
-
-		for tag, entities in self.tagged_entities.items():
-			for entity in entities:
-				if not entity.is_alive():
-					entities.remove(entity)
-
-		for entity in self.entities:
-			entity.update(delta)
-			if not entity.is_alive():
-				self.entities.remove(entity)
-				continue
-
-			y_pos = get_1d_pos(entity.pos.y, TILE_SIZE)
-
-			if y_pos not in self.sorted_entities:
-				self.sorted_entities[y_pos] = []
-
-			self.sorted_entities[y_pos].append(entity)
 
 	def get_entities(self, y_pos: int):
 		entities = []
@@ -58,3 +46,31 @@ class EntityManager:
 				entities.append(entity)
 
 		return entities
+
+	def remove_entities(self):
+		self.sorted_entities.clear()
+
+		for entity in self.entities_to_remove:
+			self.entities.remove(entity)
+
+			for tag in self.entity_tags[entity]:
+				self.tagged_entities[tag].remove(entity)
+
+		self.entities_to_remove.clear()
+
+	def update(self, delta: float):
+		self.remove_entities()
+
+		for entity in self.entities:
+			if not entity.is_alive():
+				self.remove_entity(entity)
+
+		for entity in self.entities:
+			entity.update(delta)
+
+			y_pos = get_1d_pos(entity.pos.y, TILE_SIZE)
+
+			if y_pos not in self.sorted_entities:
+				self.sorted_entities[y_pos] = []
+
+			self.sorted_entities[y_pos].append(entity)
